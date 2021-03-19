913adb07173a2e2f64080217965fcf62b403f493
erikokres <erikokresna@gmail.com>
2021-03-19 14:06:43 +0700

    Scripts

---
+++ Serampang Sendal/Assets/Scripts/Peluru.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Peluru : MonoBehaviour
+{
+     public float speed = 10f;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.forward * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
